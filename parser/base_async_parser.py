228de49e99a99c22ecdbc34407b5b493a4f4aa4c
Viachaslau_Kazakou <Viachaslau.Kazakou@coxautoinc.com>
2023-12-07 21:56:38 +0100

    add thread parser

--- parser/base_async_parser.py
+++ parser/base_async_parser.py
@@ -26,17 +26,17 @@ class BaseParser(Parser):
     title: list = None
     extracted_urls = []
 
-    def _get_page(self, page_url):
-        with httpx.Client() as client:
-            response = client.get(page_url)
+    async def _get_page(self, page_url):
+        async with httpx.AsyncClient() as client:
+            response = await client.get(page_url)
             if response.status_code == 200:
                 soup = BeautifulSoup(response.text, 'html.parser')
                 return soup
             else:
                 print(f"Failed to retrieve the page {page_url}. Status code: {response.status_code}")
 
-    def get_page_links(self, page_url):
-        if soup := self._get_page(page_url):
+    async def get_page_links(self, page_url):
+        if soup := await self._get_page(page_url):
             anchor_tags = soup.select('a.stretched-link')
             if anchor_tags:
                 for item in anchor_tags:
@@ -45,37 +45,41 @@ class BaseParser(Parser):
                 print("No anchor tag found on the page.")
             return
 
-    def get_page_title(self, extracted_url):
-        if soup := self._get_page(extracted_url):
+    async def get_page_title(self, extracted_url):
+        if soup := await self._get_page(extracted_url):
             if soup:
                 location = soup.select_one(".job-summary-location>div>ul>li").get_text()
                 title = soup.select_one('h1.hero-heading').get_text()
-                self.title.append(f"{title}. Location:[ {location} ]")
                 return f"{title}. Location:[ {location} ]"
             else:
                 print("Page not found")
 
-    def collect_links(self):
+    async def get_titles(self, extracted_urls):
+        for url in extracted_urls:
+            extracted_url = ''.join((self.url, url))
+            await self.get_page_title(extracted_url)
+
+    async def collect_links(self):
         print(self.site_url)
         for page in range(2,6):
             current_url = ''.join((self.site_url, f"/en/jobs/?page={page}"))
             print(current_url)
             print("---")
-            self.get_page_links(current_url)
+            await self.get_page_links(current_url)
         # self.extracted_urls = list(set(self.extracted_urls))
         [print(key+1, f"{self.site_url}{item}") for key, item in enumerate(self.extracted_urls)]
 
-    def process(self):
+    async def process(self):
         """
         Parse pipeline without async methods
         :return:
         """
-        self.collect_links()
+        await self.collect_links()
 
 
 if __name__ == "__main__":
     start_time = time.time()
     url = "https://jobs.coxenterprises.com"
     parser = BaseParser(url)
-    parser.process()
+    asyncio.run(parser.process())
     print(f"Execution time: {time.time()-start_time}")
